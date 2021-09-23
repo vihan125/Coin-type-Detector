c1e42a4e4650ec5bce8eedd047b7e55069ecf182
vihan melaka <vihan97@gmail.com>
2021-09-23 15:45:01 +0530

    changes in the timing diagram
    new implementation with new timing diagram according to the documentation

---
+++ FSM_CoinDetector/FSM_CoinDetector.hw/webtalk/labtool_webtalk.tcl
@@ -0,0 +1,28 @@
+webtalk_init -webtalk_dir C:/Users/Vihan/Documents/GitHub/Coin-type-Detector/FSM_CoinDetector/FSM_CoinDetector.hw/webtalk/
+webtalk_register_client -client project
+webtalk_add_data -client project -key date_generated -value "Sat Sep 18 01:26:33 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key product_version -value "Vivado v2016.3 (64-bit)" -context "software_version_and_target_device"
+webtalk_add_data -client project -key build_version -value "1682563" -context "software_version_and_target_device"
+webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
+webtalk_add_data -client project -key registration_id -value "" -context "software_version_and_target_device"
+webtalk_add_data -client project -key tool_flow -value "xsim_vivado" -context "software_version_and_target_device"
+webtalk_add_data -client project -key beta -value "FALSE" -context "software_version_and_target_device"
+webtalk_add_data -client project -key route_design -value "FALSE" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_family -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "caf4c7c5-3078-48a6-936f-094afa26eec2" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_id -value "f5a98878-32ca-4fe0-ba23-3f24e00e6d70" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
+webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2803 MHz" -context "user_environment"
+webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
+webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
+webtalk_register_client -client labtool
+webtalk_add_data -client labtool -key pgmcnt -value "00:00:00" -context "labtool\\usage"
+webtalk_add_data -client labtool -key cable -value "" -context "labtool\\usage"
+webtalk_transmit -clientid 2073300185 -regid "" -xml C:/Users/Vihan/Documents/GitHub/Coin-type-Detector/FSM_CoinDetector/FSM_CoinDetector.hw/webtalk/usage_statistics_ext_labtool.xml -html C:/Users/Vihan/Documents/GitHub/Coin-type-Detector/FSM_CoinDetector/FSM_CoinDetector.hw/webtalk/usage_statistics_ext_labtool.html -wdm C:/Users/Vihan/Documents/GitHub/Coin-type-Detector/FSM_CoinDetector/FSM_CoinDetector.hw/webtalk/usage_statistics_ext_labtool.wdm -intro "<H3>LABTOOL Usage Report</H3><BR>"
+webtalk_terminate
